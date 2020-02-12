da26bbc7ce6876e986c6caa5b7eb3715b4d50515
chen-qian-dan <54783217+chen-qian-dan@users.noreply.github.com>
2020-02-13 08:50:45 +1030

    array

---
+++ array.js
@@ -0,0 +1,38 @@
+
+let val = ['Qian', 'Chen']; 
+
+val.unshift('Dan');
+
+console.log(val); 
+
+
+
+
+
+
+
+/*
+strVal = val.pop()
+strVal = val.shift();
+val.push('Dan'); // add item to the last.
+val.pop('Dan'); // remove the last item
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
