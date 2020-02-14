d6f358626b2a389aff5690d41cacbbebc5313518
chen-qian-dan <54783217+chen-qian-dan@users.noreply.github.com>
2020-02-14 15:47:34 +1030

    object

---
+++ object.js
@@ -0,0 +1,19 @@
+
+let ourDog = {
+    'name': 'Max', 
+    'legs': 4, 
+    'tails': 1, 
+    'friends': ['everything!']
+};
+
+let val = ourDog.friends;
+let val = ourDog['name']; 
+
+// key can't be mix type. 
+let number  = {
+    1: '1', 
+    2: '2'
+}
+
+
+
