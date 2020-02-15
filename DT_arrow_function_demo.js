5b746da90476137c90a86881193ab8d425b7d5de
chen-qian-dan <54783217+chen-qian-dan@users.noreply.github.com>
2020-02-16 00:44:28 +1030

    DT_arrow_function_demo

---
+++ DT_arrow_function_demo.js
@@ -0,0 +1,20 @@
+let magic = function(arr1, arr2) {
+    return arr1.concat(arr2);
+};
+
+// the same as 
+let magic = (arr1, arr2) => {
+    return arr1.concat(arr2);
+};
+
+// the same as 
+let magic = (arr1, arr2) => arr1.concat(arr2);
+
+
+console.log(magic([1, 2], [3, 4, 5]));
+
+
+
+
+
+
